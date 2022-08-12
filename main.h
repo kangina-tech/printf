d3f12591fb805266790343885860c186ac3091df
PilotKadesh <pilotsamson@gmail.com>
2022-08-12 08:49:33 -0700

    updated now

--- main.h
+++ main.h
@@ -1,123 +1,117 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <limits.h>
-#include <stdio.h>
-#include "main.h"
-#include <stdint.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * struct buffer_s - A new type defining a buffer struct.
+ * @buffer: A pointer to a character array.
+ * @start: A pointer to the start of buffer.
+ * @len: The length of the string stored in buffer.
  */
-int main(void)
+typedef struct buffer_s
 {
-    int len = 0, len3, len1;
-    int len2 = 50, len4 = 50;
-    unsigned int ui;
-    void *addr;
-	char *ptr = "hello";
-	unsigned char(*ptr_func)(const char *, char *) = handle_flags;
-	unsigned int a;
-	char *s;
-	char *s2;
-	char *s3;
-	char *s4;
-	char c;
-	int i;
-	int b;
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
 
-	a = -17;
-	c = 'p';
+/**
+ * struct converter_s - A new type defining a converter struct.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+} converter_t;
 
-	i = -123455;
+/**
+ * struct flag_s - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
+ */
+typedef struct flag_s
+{
+	unsigned char flag;
+	unsigned char value;
+} flag_t;
 
-	s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit\n";
+int _printf(const char *format, ...);
 
-	s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
+/* Conversion Specifier Functions */
+unsigned int convert_c(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_s(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_di(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_percent(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_b(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_u(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_o(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_x(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_S(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_p(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_r(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_R(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
 
-	s3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
+/* Handlers */
+unsigned char handle_flags(const char *flags, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
 
-	s4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
+/* Modifiers */
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
 
-	b = 98;
+/* Helper Functions */
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *output);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
 
-	_printf("asdfasdfas\n");
-	printf("asdfasdfas\n");
-	_printf("--------------\n\n\n");
-	_printf("fasdf\n %s\n %c\n %s\n %s\n %s\n %i\n %b\n\n\n", s, c, s2, s3, s4, i, b);
-	printf("fasdf\n %s\n %c\n %s\n %s\n %s\n %i\n\n\n", s, c, s2, s3, s4, i);
-	_printf("--------------\n\n");
-	_printf("%b\n", 98);
-	_printf("--------------\n\n\n");
-
-	len = printf("%u\n", a);
-	len1 = _printf("%u\n", a);
-	printf("%i\n", len);
-	_printf("%i\n", len1);
-	_printf("%b, %b, %b\n", 98, INT_MAX, 1001101);
-	printf("testing pointers: %p, %p, %p \n", ptr, ptr_func, NULL);
-	_printf("testing pointers: %p, %p, %p\n", ptr, ptr_func, NULL);
-	len = _printf("Characters: %c %c, %cdone.\n", 'a', NULL, 78);
- 	len2 = printf("Characters: %c %c, %cdone.\n", 'a', NULL, 78);
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Decimals: %d %ld done\n", 1977, 650000L);
-	_printf("Some different radicals: %d %x %X done.\n", 100, 100, 100);
-	printf("Some different radicals: %d %x %X done.\n", 100, 100, 100);
-	_printf("Some different radicals: %x %x %X done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	printf("Some different radicals: %x %x %X done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	_printf("Some different radicals: %o %o %o done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	printf("Some different radicals: %o %o %o done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	_printf("Some different radicals: %u %u %u done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	printf("Some different radicals: %u %u %u done.\n", (unsigned int)INT_MAX + 1024, INT_MIN, INT_MAX);
-	_printf("%s \n", "Alx is Amaizing");
-	_printf("testing other possible integer: %d %d\n", INT_MAX, INT_MIN);
-    printf("testing other possible integer: %d %d\n", INT_MAX, INT_MIN);
-	len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    len4 = printf(NULL);
-	len3 = _printf(NULL);
-	printf("%d\n", len4);
-	_printf("%d\n", len3);
-	ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i, %d]\n", len, len, len3);
-    printf("Length:[%d, %i, %d]\n", len2, len2, len4);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u, %u]\n", ui, INT_MIN);
-    printf("Unsigned:[%u, %u]\n", ui, INT_MIN);
-    len = _printf("Unsigned octal:[%o, %o]\n", ui, NULL);
-    len1 = printf("Unsigned octal:[%o, %o]\n", ui, NULL);
-    len2 = _printf("Unsigned hexadecimal:[%x, %x, %X]\n", ui, NULL, ui);
-    len3 =  printf("Unsigned hexadecimal:[%x, %x, %X]\n", ui, NULL, ui);
-    _printf("Length:[%d, %i]\n", len, len2);
-	printf("Length:[%d, %i]\n", len1, len3);
-	len = _printf("String:[%s, %s]\n", "I am a string !", NULL);
-    len1 = printf("String:[%s, %s]\n", "I am a string !", NULL);
-	printf("%d\n", len1);
-	_printf("%d\n", len);
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%c%%%c%%]\n", 'g', 'r');
-    len2 = printf("Percent:[%c%%%c%%]\n", 'g', 'r');
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n", "mahadi");
-    _printf("Unknown:[%R]\n", "mahadi");
-
-    _printf("\nTesting: [%S]\n", "Best\nSchool");
-    printf("Hash: [%#x, %#X, %#o]\n", 969255, 969255, 969255);
-    _printf("Hash: [%#x, %#X, %#o]\n", 969255, 969255, 969255);
-
-    printf("space and +: [% 05d, %+i, %+10d]\n", 258455, 258455, 258455);
-    _printf("space and +: [% 05d, %+i, %+10d]\n", 258455, 258455, 258455);
-
-    printf("space and +: [% 05d, %+i, %+10d]\n", 255, 255, 255);
-    _printf("space and +: [% 05d, %+i, %+10d]\n", 255, 255, 255);
-    printf("width: [%6X, %6x, %10s]\n",5, -4568,"ab");
-    _printf("width: [%6X, %6x, %10s]\n",5, -4568,"ab");
-    _printf("precision: [%012.4d, %.5i, %.1u]\n", 231, 4, 67);
-	printf("precision: [%012.4f, %.5f, %.1f]\n", 231.0, 4.0, 67.0);
-    return (0);
-}
+#endif /* MAIN_H */
 
